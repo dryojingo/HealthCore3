aee069f01407793bc34eba50d53a15e38410ba71
Medhat Elmasry <medhat@elmasry.ca>
2019-10-12 02:19:01 -0700

    1st commit

---
+++ HealthAPI/Data/HealthContext.cs
@@ -0,0 +1,23 @@
+using HealthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthAPI.Data
+{public class HealthContext : DbContext {
+    public HealthContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+        builder.Entity<Ailment>().Property(p => p.Name).HasMaxLength(40);
+        builder.Entity<Medication>().Property(p => p.Name).HasMaxLength(40);
+        builder.Entity<Patient>().Property(p => p.Name).HasMaxLength(40);
+
+        builder.Entity<Ailment>().ToTable("Ailment");
+        builder.Entity<Medication>().ToTable("Medication");
+        builder.Entity<Patient>().ToTable("Patient");
+    }
+
+    public DbSet<Ailment> Ailments { get; set; }
+    public DbSet<Medication> Medications { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+}
+
+}
